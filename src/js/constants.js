9d675d4f3a78497d5ef5d8e6aab95892b6a6b0ef
tooooo1 <wer4272@nate.com>
2022-07-05 01:30:54 +0900

    Feat: 제한사항 추가

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,2 @@
-export const PLUS = "+";
-export const MINUS = "-";
-export const MULTIPLICATION = "X";
-export const DIVISION = "/";
-
-export const MAX_LENGTH = 3;
+export const MAX_NUMBER_LENGTH = 3;
+export const MAX_OPER_LENGTH = 1;
