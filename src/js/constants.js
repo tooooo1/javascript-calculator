e53a2b0e4b99e1feda01193be22279bc61759767
tooooo1 <wer4272@nate.com>
2022-07-05 02:57:06 +0900

    Feat: constant 텍스트 추가

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,2 +1,9 @@
 export const MAX_NUMBER_LENGTH = 3;
 export const MAX_OPER_LENGTH = 1;
+
+export const INVALID_LENGTH = `숫자는 ${MAX_NUMBER_LENGTH}개가 최대에요`;
+export const INVALID_OPER_LENGTH = `${
+  MAX_OPER_LENGTH + 1
+}개의 숫자에 대해서만 가능해요`;
+
+export const REQUIRED_DIGIT = "숫자를 먼저 입력해주세요";
