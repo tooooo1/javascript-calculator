3985df5fd8398740d0b97ad293d79166e7de62c1
tooooo1 <wer4272@nate.com>
2022-07-05 01:30:39 +0900

    Feat: 계산 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,68 @@
-import { PLUS, MINUS, MULTIPLICATION, DIVISION } from "./constant";
+import { MAX_NUMBER_LENGTH, MAX_OPER_LENGTH } from "./constants.js";
+
 const calcValue = document.querySelector("#total");
-const calcDigit = document.querySelectorAll(".digits");
+const calcDigit = document.querySelectorAll(".calculator");
 const calculatorValue = {
-  value: 0,
-  count: 0,
+  value: "",
+  operCount: 0,
+  numberCount: 0,
 };
 
 const renderNumber = (number) => {
   calcValue.textContent = number;
 };
 
-const calculator = () => {
-  calcDigit.forEach((button) => {
-    button.addEventListener("click");
-  });
+const reset = () => {
+  calculatorValue.value = "";
+  calculatorValue.operCount = 0;
+  calculatorValue.numberCount = 0;
+  renderNumber(0);
 };
 
-const calculatorReducer = (operator, state) => {
-  switch (operator) {
-    case PLUS: {
-      calculatorValue.value += state;
-      count += 1;
+const setNumber = (value, type) => {
+  switch (type) {
+    case "number":
+      if (calculatorValue.numberCount >= MAX_NUMBER_LENGTH)
+        return alert(`숫자는 ${MAX_NUMBER_LENGTH}개가 최대에요`);
+
+      calculatorValue.numberCount += 1;
       break;
-    }
-    case MINUS: {
-      calculatorValue.value += state;
-      count += 1;
+    case "oper":
+      if (calculatorValue.operCount >= MAX_OPER_LENGTH)
+        return alert(`${MAX_OPER_LENGTH + 1}개의 숫자에 대해서만 가능해요`);
+
+      calculatorValue.operCount += 1;
+      calculatorValue.numberCount = 0;
       break;
-    }
-    case MULTIPLICATION: {
-      calculatorValue.value += state;
-      count += 1;
+    default:
+      alert("정확한 값을 입력해주세요");
       break;
-    }
-    case DIVISION: {
-      calculatorValue.value += state;
-      count += 1;
-      break;
-    }
   }
+
+  calculatorValue.value += value;
+  renderNumber(calculatorValue.value);
 };
 
-export default calculator;
+const setResult = () => {
+  const resultValue = eval(calculatorValue.value);
+  renderNumber(resultValue);
+};
+
+const handleClickBtn = (e) => {
+  const clickValue = e.target.innerText;
+
+  if (clickValue === "AC") return reset();
+  if (clickValue === "=") return setResult();
+
+  if (/[0-9]/.test(clickValue)) return setNumber(clickValue, "number");
+  setNumber(clickValue, "oper");
+};
+
+const init = () => {
+  calcDigit.forEach((button) => {
+    button.addEventListener("click", handleClickBtn);
+  });
+  renderNumber(0);
+};
+
+init();
